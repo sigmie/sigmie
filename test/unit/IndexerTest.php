53561ab903684e3b2898e42902768181cfe2ee82
Nico orfi <nico_orfi@yahoo.com>
2019-09-13 06:59:58 +0200

    Add xdebug - vscode configuration

---
+++ test/unit/IndexerTest.php
@@ -0,0 +1,14 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+
+final class ArticleUpdateTest  extends TestCase
+{
+   /**
+    * @test
+    */
+    public function testFoo()
+    {
+        $this->assertFalse(true);
+    }
+}
