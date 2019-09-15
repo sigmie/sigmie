6c33fb623f655011ebd2d0d538068d3dd977ad6a
Nico orfi <nico_orfi@yahoo.com>
2019-09-15 08:26:56 +0200

    Refactor namespaces

--- test/unit/Service/ClientTest.php
+++ test/unit/Service/ClientTest.php
@@ -1,22 +1,19 @@
 <?php
 
-use niElastic\Service\Client;
+namespace Ni\Elastic\Test\Service;
 
+use Ni\Elastic\Service\Client;
 use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        $this->client = new Client();
-    }
-
     /**
      * @test
      */
     public function clientDefaultHost(): void
     {
-        $this->assertEquals($this->client->gethost(), '127.0.0.1');
+        $client = new Client();
+        $this->assertEquals($client->gethost(), '127.0.0.1');
     }
 
     /**
@@ -24,6 +21,7 @@ class ClientTest extends TestCase
      */
     public function clientDefaultPort(): void
     {
-        $this->assertEquals($this->client->getPort(), '9200');
+        $client = new Client();
+        $this->assertEquals($client->getPort(), '9200');
     }
 }
