b16d4e25c2d4cda013f9865789cefafee8a12c2c
Nico orfi <nico_orfi@yahoo.com>
2019-09-27 08:12:29 +0200

    Set Index and Type dependecies in build method for Manager class

---
+++ src/Index/BaseIndex.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ni\Elastic\Index;
+
+use Ni\Elastic\FundamentalTrait;
+
+class BaseIndex
+{
+    use FundamentalTrait;
+
+    public function __construct()
+    {
+        //TODO implement
+    }
+}
