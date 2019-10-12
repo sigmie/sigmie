e20d43467e863f7b21270a2216b472c8453a16dc
Nico orfi <nico_orfi@yahoo.com>
2019-10-12 19:25:52 +0200

    UPD: Refactor Response and Action abstractions

--- src/Index/Response/Listing.php
+++ src/Index/Response/Listing.php
@@ -3,10 +3,11 @@
 namespace Ni\Elastic\Index\Response;
 
 use Ni\Elastic\Collection;
+use Ni\Elastic\Contract\Response;
 use Ni\Elastic\Contract\Response\Listing as ListingResponse;
 use Ni\Elastic\Index\IndexCollection;
 
-class Listing implements ListingResponse
+class Listing implements Response
 {
     public function result(array $response): Collection
     {
--- src/Index/Response/Remove.php
+++ src/Index/Response/Remove.php
@@ -2,9 +2,10 @@
 
 namespace Ni\Elastic\Index\Response;
 
+use Ni\Elastic\Contract\Response;
 use Ni\Elastic\Contract\Response\Remove as RemoveResponse;
 
-class Remove implements RemoveResponse
+class Remove implements Response
 {
     public function result(array $response): bool
     {
