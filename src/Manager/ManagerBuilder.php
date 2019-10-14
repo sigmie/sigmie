032bc69b213d32421ed9e6f153e0100be3f744c9
Nico orfi <nico_orfi@yahoo.com>
2019-10-14 06:38:23 +0200

    UPD: Refactor manager builder

--- src/Manager/ManagerBuilder.php
+++ src/Manager/ManagerBuilder.php
@@ -6,8 +6,11 @@ use Elasticsearch\Client as Elasticsearch;
 use Ni\Elastic\Index\Manager as IndexManager;
 use Ni\Elastic\Manager\Manager;
 use Ni\Elastic\ActionDispatcher;
+use Ni\Elastic\Contract\ActionDispatcher as ActionDispatcherInterface;
+use Ni\Elastic\Contract\ResponseHandler as ResponseHandlerInterface;
 use Ni\Elastic\ResponseHandler;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class ManagerBuilder
 {
@@ -18,10 +21,25 @@ class ManagerBuilder
      */
     private $elasticsearch;
 
+    /**
+     * Response handler
+     *
+     * @var ResponseHandlerInterface
+     */
     private $responseHandler = null;
 
+    /**
+     * Event dispatcher
+     *
+     * @var EventDispatcherInterface
+     */
     private $eventDispatcher = null;
 
+    /**
+     * Action dispatcher
+     *
+     * @var ActionDispatcherInterface
+     */
     private $actionDispatcher = null;
 
     public function __construct(Elasticsearch $elasticsearch)
@@ -29,6 +47,11 @@ class ManagerBuilder
         $this->elasticsearch = $elasticsearch;
     }
 
+    /**
+     * Initialize the manager class
+     *
+     * @return Manager
+     */
     public function build(): Manager
     {
         $manager = new Manager();
@@ -37,21 +60,45 @@ class ManagerBuilder
         return $manager;
     }
 
+    private function responseHandler(): ResponseHandlerInterface
+    {
+        if ($this->responseHandler instanceof ResponseHandlerInterface) {
+            return $this->responseHandler;
+        }
+
+        $this->responseHandler = new ResponseHandler();
+
+        return $this->responseHandler;
+    }
+
+    private function eventDispatcher(): EventDispatcherInterface
+    {
+        if ($this->eventDispatcher instanceof EventDispatcherInterface) {
+            return $this->eventDispatcher;
+        }
+
+        $this->eventDispatcher = new EventDispatcher();
+
+        return $this->eventDispatcher;
+    }
+
+    private function actionDispatcher(): ActionDispatcherInterface
+    {
+        if ($this->actionDispatcher instanceof ActionDispatcherInterface) {
+            return $this->actionDispatcher;
+        }
+
+        $this->actionDispatcher = new ActionDispatcher($this->elasticsearch, $this->eventDispatcher());
+
+        return $this->actionDispatcher;
+    }
+
     public function newIndexManager(): IndexManager
     {
-        if ($this->eventDispatcher === null) {
-            $this->eventDispatcher = new EventDispatcher();
-        }
+        $actionDispatcher = $this->actionDispatcher();
+        $responseHandler = $this->responseHandler();
 
-        if ($this->responseHandler === null) {
-            $this->responseHandler = new ResponseHandler();
-        }
-
-        if ($this->actionDispatcher === null) {
-            $this->actionDispatcher = new ActionDispatcher($this->elasticsearch, $this->eventDispatcher);
-        }
-
-        $manager = new IndexManager($this->actionDispatcher, $this->responseHandler);
+        $manager = new IndexManager($actionDispatcher, $responseHandler);
 
         return $manager;
     }
