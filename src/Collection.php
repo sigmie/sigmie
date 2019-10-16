a6fb739daa7f04bab847889aebfb3cde1739f95e
Nico orfi <nico_orfi@yahoo.com>
2019-10-16 05:33:50 +0200

    UPD: Add collection doc blocks

--- src/Collection.php
+++ src/Collection.php
@@ -9,14 +9,32 @@ use ArrayIterator;
 
 abstract class Collection implements IteratorAggregate, Countable, ArrayAccess
 {
+    /**
+     * Collection elements
+     *
+     * @var array
+     */
     private $elements;
 
-    public function __construct(array $list)
+    /**
+     * Collection constructor
+     *
+     * @param array $elements
+     */
+    public function __construct(array $elements)
     {
-        $this->elements = $list;
+        $this->elements = $elements;
     }
 
-    public function offsetSet($offset, $value)
+    /**
+     * Set offset method
+     *
+     * @param string $offset
+     * @param Element $value
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->elements[] = $value;
@@ -26,31 +44,67 @@ abstract class Collection implements IteratorAggregate, Countable, ArrayAccess
         $this->elements[$offset] = $value;
     }
 
-    public function offsetExists($offset)
+    /**
+     * Offset exists method
+     *
+     * @param string $offset
+     *
+     * @return bool
+     */
+    public function offsetExists($offset): bool
     {
         return isset($this->elements[$offset]);
     }
 
+    /**
+     * Unset offset method
+     *
+     * @param string $offset
+     *
+     * @return void
+     */
     public function offsetUnset($offset)
     {
         unset($this->elements[$offset]);
     }
 
-    public function offsetGet($offset)
+    /**
+     * Get offset
+     *
+     * @param string $offset
+     *
+     * @return Element|null
+     */
+    public function offsetGet($offset): ?Element
     {
         return isset($this->elements[$offset]) ? $this->elements[$offset] : null;
     }
 
-    public function getIterator()
+    /**
+     * Iterator method
+     *
+     * @return ArrayIterator
+     */
+    public function getIterator() : ArrayIterator
     {
         return new ArrayIterator($this->elements);
     }
 
-    public function count()
+    /**
+     * Count method
+     *
+     * @return int
+     */
+    public function count(): int
     {
         return count($this->elements);
     }
 
+    /**
+     * First element method
+     *
+     * @return Element
+     */
     public function first(): Element
     {
         return $this->elements[0];
