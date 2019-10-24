214a8b843924858ee9601038c9ce44b623abbc66
Nico orfi <nico_orfi@yahoo.com>
2019-10-24 07:03:19 +0200

    UPD: Document abstract class properties

---
+++ test/Unit/TypesTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Sigma\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TypesTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function foo(): void
+    {
+        $this->assertTrue(false);
+    }
+}
