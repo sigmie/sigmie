b1f1f4c851971c8f8aba045fcf5d6ba589fcde7e
Nico orfi <nico_orfi@yahoo.com>
2019-11-04 14:49:06 +0100

    UPD: Element factory

---
+++ src/ElementFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sigma;
+
+class ElementFactory
+{
+    public function createDocument()
+    { }
+
+    public function createIndex()
+    { }
+
+    public function createMappings()
+    { }
+}
