343190c87cd4d42bb62175b5b8e2436b083b4827
Nico orfi <nico_orfi@yahoo.com>
2019-11-25 07:01:14 +0100

    UPD: App layout navbar and sidebar style

---
+++ resources/views/common/navbar.blade.php
@@ -0,0 +1,15 @@
+                <div class="m-0 p-3 row flex h-full">
+
+                    <div class="col-md-4 align-middle">
+                        <input id="search" name="search" type="search" class="align-middle">
+                    </div>
+
+                    <div class="col-md-4 col-md-offset-4 flex flex-col justify-center">
+                        <div class="flex justify-end">
+                            <div class="pr-3">
+                                Logout
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
---
+++ resources/views/common/sidebar.blade.php
@@ -0,0 +1,50 @@
+                <div class="m-0 h-full p-3 row flex">
+
+                    <div class="col-md-12 align-middle mb-8">
+                        <img src="https://res.cloudinary.com/markos-nikolaos-orfanos/image/upload/v1574659602/Group_2_fxapdw.png" width="200">
+                    </div>
+
+                    <div class="col-md-12 flex flex-col justify-center">
+                        <div class="py-2 leading-relaxed">
+                            <h3 class="uppercase font-semibold text-gray-600 pb-4">
+                                Basics
+                            </h3>
+                            <ul class="pl-3">
+                                <li>
+                                    <span class="inline-block text-gray-100 h-full">
+                                        <svg class="h-10" width="20px" height="20px" viewBox="0 0 25 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                            <g id="Layout" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                <g id="Desktop-HD" transform="translate(-52.000000, -355.000000)">
+                                                    <rect id="Rectangle-2" fill="#313758" x="0" y="0" width="325" height="1024"></rect>
+                                                    <g id="Group-9" transform="translate(52.000000, 355.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                                                        <g id="icon-server">
+                                                            <path d="M2.77777778,0 L22.2222222,0 C23.7563465,0 25,1.24365347 25,2.77777778 L25,22.2222222 C25,23.7563465 23.7563465,25 22.2222222,25 L2.77777778,25 C1.24365347,25 0,23.7563465 0,22.2222222 L0,2.77777778 C0,1.25 1.25,0 2.77777778,0 Z M22.2222222,11.1111111 L22.2222222,2.77777778 L2.77777778,2.77777778 L2.77777778,11.1111111 L22.2222222,11.1111111 Z M22.2222222,13.8888889 L2.77777778,13.8888889 L2.77777778,22.2222222 L22.2222222,22.2222222 L22.2222222,13.8888889 Z M6.94444444,8.33333333 C6.17738229,8.33333333 5.55555556,7.7115066 5.55555556,6.94444444 C5.55555556,6.17738229 6.17738229,5.55555556 6.94444444,5.55555556 C7.7115066,5.55555556 8.33333333,6.17738229 8.33333333,6.94444444 C8.33333333,7.7115066 7.7115066,8.33333333 6.94444444,8.33333333 Z M6.94444444,19.4444444 C6.17738229,19.4444444 5.55555556,18.8226177 5.55555556,18.0555556 C5.55555556,17.2884934 6.17738229,16.6666667 6.94444444,16.6666667 C7.7115066,16.6666667 8.33333333,17.2884934 8.33333333,18.0555556 C8.33333333,18.8226177 7.7115066,19.4444444 6.94444444,19.4444444 Z" id="Shape"></path>
+                                                        </g>
+                                                    </g>
+                                                </g>
+                                            </g>
+                                        </svg>
+                                    </span>
+                                    <span class="inline-block text-gray-100 font-semibold h-10">
+                                        Nodes
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-12 flex flex-col justify-center">
+                        <div class="pr-3">
+                            <h3 class="uppercase font-semibold text-gray-600">
+                                Resources
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-md-12 flex flex-col justify-center">
+                        <div class="pr-3">
+                            <h3 class="uppercase font-semibold text-gray-600">
+                                Profile
+                            </h3>
+                        </div>
+                    </div>
+
+                </div>
