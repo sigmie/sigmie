c9c113f6ba96aba72f4efa77d597d94ed49b2b1b
Nico <nico_orfi@yahoo.com>
2020-01-06 20:34:16 +0100

    TEST: NewsletterSubscription

--- tests/Unit/NewsletterSubscriptionTest.php
+++ tests/Unit/NewsletterSubscriptionTest.php
@@ -10,7 +10,7 @@ class NewsletterSubscriptionTest extends TestCase
     /**
      * @test
      */
-    public function confirmed_attribute_cast_to_boolean(): void
+    public function confirmed_attribute_casts_to_boolean(): void
     {
         $newsletterSubscription = new NewsletterSubscription(['confirmed' => 1]);
 
@@ -18,8 +18,8 @@ class NewsletterSubscriptionTest extends TestCase
     }
 
     /**
-    * @test
-    */
+     * @test
+     */
     public function confirmed_default_value_is_false(): void
     {
         $newsletterSubscription = new NewsletterSubscription();
