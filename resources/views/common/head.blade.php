31473076952ae6fe4969cd8dea5552aa079addd0
Nico <nico_orfi@yahoo.com>
2020-02-14 06:57:06 +0100

    UPD: rename node dev container to echo

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -8,7 +8,7 @@
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png')}}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png')}}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png"')}}">
-<link rel=" manifest" href="{{ asset('/site.webmanifest')}}">
+<link rel=" manifest" href="{{ asset('/manifest.json')}}">
 
 <title>{{ config('app.name', 'Laravel') }}</title>
 
