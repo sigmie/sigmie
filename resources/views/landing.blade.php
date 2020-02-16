33cd27c02f68601b54879afc795ac8afcc8d0442
Nico <nico_orfi@yahoo.com>
2020-02-16 10:36:26 +0100

    WIP

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -44,21 +44,25 @@
 
             <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
                 id="nav-content">
-                {{-- <ul class="list-reset lg:flex justify-end flex-1 items-center">
-                    <li class="mr-3">
+                <ul class="list-reset lg:flex justify-end flex-1 items-center">
+                    {{-- <li class="mr-3">
                         <a class="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Active</a>
                     </li>
                     <li class="mr-3">
                         <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
                             href="#">link</a>
-                    </li>
+                    </li> --}}
                     <li class="mr-3">
                         <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                            href="#">link</a>
+                            href="https://github.com/mos-sigma/sigma">
+                            <svg role="img" class="h-8" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                            </svg></a>
                     </li>
-                </ul> --}}
+                </ul>
                 {{-- <button --}}
-                    {{-- class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">Action</button> --}}
+                {{-- class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">Action</button> --}}
             </div>
         </div>
     </nav>
@@ -777,17 +781,38 @@
                 <div class="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
                     <div class="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
                         <div class="p-8 text-3xl font-bold text-center border-b-4">Hobby</div>
-                        <ul class="w-full text-center text-sm">
-                            <li class="border-b py-4">15 days free trial</li>
-                            <li class="border-b py-4">Alerts- we monitor your clusters and even send you mail
-                                notification about anything suspicious.</li>
-                            <li class="border-b py-4">3 Node cluster for hight availability.</li>
-                            <li class="border-b py-4">Metrics we provide metrics about your Elasticseasrch data and
-                                indexes.</li>
+                        <ul class="w-full text-sm px-10 py-2">
+                            <li class="py-4">
+                                <span class="pr-3">
+                                    <icon-check height="18px" width="18px" />
+                                </span>
+                                15 days free trial
+                            </li>
+                            <li class="py-4">
+                                <span class="pr-3">
+                                    <icon-check height="18px" width="18px" />
+                                </span>
+                                Alerts and notifications
+                            </li>
+                            <li class="py-4">
+                                <span class="pr-3">
+                                    <icon-check height="18px" width="18px" />
+                                </span>
+                                Up to 3 Nodes</li>
+                            <li class="py-4">
+                                <span class="pr-3">
+                                    <icon-check height="18px" width="18px" />
+                                </span>
+                                Metris and analytics</li>
+                            <li class="py-4">
+                                <span class="pr-3">
+                                    <icon-check height="18px" width="18px" />
+                                </span>
+                                Playgound function</li>
                         </ul>
                     </div>
-                    <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                        <div class="w-full pt-6 text-3xl text-gray-600 font-bold text-center">$12
+                    <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow px-6">
+                        <div class="w-full text-3xl pt-2 text-gray-600 font-bold text-center">$12
                         </div>
                         <div class="text-gray-400 text-sm font-normal text-center">Price doesn't include your GCP costs.
                         </div>
