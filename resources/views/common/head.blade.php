9b60014a2d8b533a0566329ff9a616768da013a3
Nico <nico_orfi@yahoo.com>
2020-02-20 10:22:24 +0100

    UPD: new favicons

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -10,11 +10,15 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<!-- CSRF Token -->
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png')}}">
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('/apple-touch-icon.png')}}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png')}}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png"')}}">
-<link rel=" manifest" href="{{ asset('/manifest.json')}}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png')}}">
+<link rel="manifest" href="{{ asset('/site.webmanifest') }}">
+<link rel="mask-icon" href="{{ asset('/safari-pinned-tab.svg')}}" color="#5bbad5">
+
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
 
 <title>{{ config('app.name', 'Laravel') }}</title>
 
