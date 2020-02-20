6b515fc4246112c090d7ae0f42db2e70d5cc7c3d
Nico <nico_orfi@yahoo.com>
2020-02-20 11:06:40 +0100

    FIX: remove debug mode from environmental variables, change the mail sender address

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'postmaster@mg.mos-sigma.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'nicoorfi@mos-sigma.com'),
         'name' => env('MAIL_FROM_NAME', 'Nico'),
     ],
 
