f125e0fa2258113f1657740cf6fc14df96cebb4c
Nico <nico_orfi@yahoo.com>
2020-03-11 15:34:40 +0100

    UPD: Make sidebar items vue data

--- resources/js/views/common/sidebar.vue
+++ resources/js/views/common/sidebar.vue
@@ -3,125 +3,26 @@
     <div class="hidden md:flex md:flex-shrink-0">
       <div class="flex flex-col w-64">
         <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-          <logo-white height="50px" width="159px" class="mx-auto py-1" />
+          <logo-white height="50px" width="159px" class="mx-auto py-1"></logo-white>
         </div>
         <div class="h-0 flex-1 flex flex-col overflow-y-auto">
           <nav class="flex-1 px-2 py-4 bg-gray-800">
             <a
-              href="#"
-              class="group flex items-center px-2 py-2 text-sm leading-5 font-medium text-white rounded-md bg-gray-900 focus:outline-none focus:bg-gray-700 transition ease-in-out duration-150"
+              v-for="(item, index) in items"
+              :key="index"
+              :href="item.href"
+              :class="[path == item.href ? 'text-white bg-gray-900 focus:outline-none' : 'mt-1 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white']"
+              class="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md focus:bg-gray-700 transition ease-in-out duration-150"
             >
-              <svg
-                class="mr-3 h-6 w-6 text-gray-300 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
+              <component
+                :class="[ path == item.href ? 'text-gray-400': 'text-gray-300']"
+                class="mr-4 h-6 w-6 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
                 stroke="currentColor"
                 fill="none"
                 viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 12l9-9 9 9M5 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10M9 21h6"
-                />
-              </svg>
-              Dashboard
-            </a>
-
-            <router-link to="/bar">Go to Bar</router-link>
-            <a
-              href
-              class="mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <svg
-                class="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              Team
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <svg
-                class="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                />
-              </svg>
-              Projects
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <svg
-                class="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                />
-              </svg>
-              Calendar
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <svg
-                class="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                />
-              </svg>
-              Documents
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <svg
-                class="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                />
-              </svg>
-              Reports
+                :is="'icon-'+item.icon"
+              ></component>
+              {{ item.text }}
             </a>
           </nav>
         </div>
@@ -154,75 +55,26 @@
           </button>
         </div>
         <div class="flex-shrink-0 flex items-center h-16 px-4 bg-gray-900">
-          <logo-white height="50px" width="159px" class="mx-auto py-1" />
+          <logo-white height="50px" width="159px" class="mx-auto py-1"></logo-white>
         </div>
         <div class="flex-1 h-0 overflow-y-auto">
           <nav class="px-2 py-4">
             <a
-              href="#"
-              class="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white bg-gray-900 focus:outline-none focus:bg-gray-700 transition ease-in-out duration-150"
+              v-for="(item, index) in items"
+              :key="index"
+              :href="item.href"
+              :class="[path == item.href ? 'text-white bg-gray-900 focus:outline-none' : 'mt-1 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white']"
+              class="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md focus:bg-gray-700 transition ease-in-out duration-150"
             >
-              <icon-home
-                class="mr-4 h-6 w-6 text-gray-300 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
+              <component
+                :class="[ path == item.href ? 'text-gray-400': 'text-gray-300']"
+                class="mr-4 h-6 w-6 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
                 stroke="currentColor"
                 fill="none"
                 viewBox="0 0 24 24"
-              ></icon-home>Dashboard
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <icon-team
-                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              ></icon-team>Team
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <icon-folder
-                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              ></icon-folder>Projects
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <icon-calendar
-                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              ></icon-calendar>Calendar
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <icon-inbox
-                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              ></icon-inbox>Documents
-            </a>
-            <a
-              href="#"
-              class="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-            >
-              <icon-report
-                class="mr-4 h-6 w-6 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              ></icon-report>Reports
+                :is="'icon-'+item.icon"
+              ></component>
+              {{ item.text }}
             </a>
           </nav>
         </div>
@@ -235,7 +87,40 @@
 export default {
   data() {
     return {
-      sidebar: "closed"
+      sidebar: "closed",
+      path: "",
+      items: [
+        {
+          text: "Dashboard",
+          href: "/home",
+          icon: "home"
+        },
+        {
+          text: "Team",
+          href: "#",
+          icon: "team"
+        },
+        {
+          text: "Projects",
+          href: "#",
+          icon: "folder"
+        },
+        {
+          text: "Calendar",
+          href: "#",
+          icon: "calendar"
+        },
+        {
+          text: "Documents",
+          href: "#",
+          icon: "inbox"
+        },
+        {
+          text: "Reports",
+          href: "#",
+          icon: "report"
+        }
+      ]
     };
   },
   methods: {
@@ -245,6 +130,9 @@ export default {
     open() {
       this.sidebar = "open";
     }
+  },
+  beforeMount() {
+    this.path = this.$route.path;
   }
 };
 </script>
