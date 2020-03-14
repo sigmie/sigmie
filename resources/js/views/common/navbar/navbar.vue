ba9faeac0f70e9a9c4220ceeb06ad8a647ecf611
Nico <nico_orfi@yahoo.com>
2020-03-14 10:40:28 +0100

    UPD: add sensitive files to .gitignore

--- resources/js/views/common/navbar/navbar.vue
+++ resources/js/views/common/navbar/navbar.vue
@@ -64,11 +64,40 @@
                       <div class="flex items-center justify-between">
                         <div
                           class="text-sm leading-5 font-medium text-orange-600 truncate"
-                        >Subscribed</div>
+                        >Removed</div>
                         <div class="ml-2 flex-shrink-0 flex">
                           <span
-                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-green-800"
-                          >#project_name</span>
+                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800"
+                          >#sigma_search</span>
+                        </div>
+                      </div>
+                      <div class="mt-2 sm:flex sm:justify-between">
+                        <div class="sm:flex">
+                          <div
+                            class="mr-6 flex items-center text-sm leading-5 text-gray-500"
+                          >Lorem, ipsum dolor sit amet cons.</div>
+                        </div>
+                        <div class="mt-2 flex items-center text-sm leading-5 text-gray-400 sm:mt-0">
+                          <span class="w-12 text-xs">
+                            <time datetime="2020-01-07">3 days</time>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li class="border-t border-gray-200">
+                  <a
+                    href="#"
+                    class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
+                  >
+                    <div class="px-4 py-4 sm:px-6">
+                      <div class="flex items-center justify-between">
+                        <div class="text-sm leading-5 font-medium text-gray-600 truncate">Created</div>
+                        <div class="ml-2 flex-shrink-0 flex">
+                          <span
+                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800"
+                          >#sigma_search</span>
                         </div>
                       </div>
                       <div class="mt-2 sm:flex sm:justify-between">
@@ -94,128 +123,23 @@
                     <div class="px-4 py-4 sm:px-6">
                       <div class="flex items-center justify-between">
                         <div
-                          class="text-sm leading-5 font-medium text-indigo-600 truncate"
-                        >Back End Developer</div>
+                          class="text-sm leading-5 font-medium text-orange-600 truncate"
+                        >Subscribed</div>
                         <div class="ml-2 flex-shrink-0 flex">
                           <span
-                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
-                          >Full-time</span>
+                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-green-800"
+                          >#project_name</span>
                         </div>
                       </div>
                       <div class="mt-2 sm:flex sm:justify-between">
                         <div class="sm:flex">
-                          <div class="mr-6 flex items-center text-sm leading-5 text-gray-500">
-                            <svg
-                              class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"
-                              />
-                            </svg>
-                            Engineering
-                          </div>
                           <div
-                            class="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mt-0"
-                          >
-                            <svg
-                              class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
-                            Remote
-                          </div>
+                            class="mr-6 flex items-center text-sm leading-5 text-gray-500"
+                          >Lorem, ipsum dolor sit amet cons.</div>
                         </div>
                         <div class="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mt-0">
-                          <svg
-                            class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                              clip-rule="evenodd"
-                            />
-                          </svg>
-                          <span>
-                            Closing on
-                            <time datetime="2020-01-07">January 7, 2020</time>
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li class="border-t border-gray-200">
-                  <a
-                    href="#"
-                    class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
-                  >
-                    <div class="px-4 py-4 sm:px-6">
-                      <div class="flex items-center justify-between">
-                        <div
-                          class="text-sm leading-5 font-medium text-indigo-600 truncate"
-                        >Back End Developer</div>
-                        <div class="ml-2 flex-shrink-0 flex">
-                          <span
-                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
-                          >Full-time</span>
-                        </div>
-                      </div>
-                      <div class="mt-2 sm:flex sm:justify-between">
-                        <div class="sm:flex">
-                          <div class="mr-6 flex items-center text-sm leading-5 text-gray-500">
-                            <svg
-                              class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"
-                              />
-                            </svg>
-                            Engineering
-                          </div>
-                          <div
-                            class="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mt-0"
-                          >
-                            <svg
-                              class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
-                            Remote
-                          </div>
-                        </div>
-                        <div class="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mt-0">
-                          <svg
-                            class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                              clip-rule="evenodd"
-                            />
-                          </svg>
-                          <span>
-                            Closing on
-                            <time datetime="2020-01-07">January 7, 2020</time>
+                          <span class="w-12">
+                            <time datetime="2020-01-07">3 days</time>
                           </span>
                         </div>
                       </div>
