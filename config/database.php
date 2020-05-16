37984aa75dde871fedf14909a2365424525a5838
Nico <nico_orfi@yahoo.com>
2020-05-16 22:34:32 +0200

    UPD: build docker image to run migrations

--- config/database.php
+++ config/database.php
@@ -132,7 +132,7 @@ return [
             'password' => env('REDIS_PASSWORD', ''),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
-            'read_write_timeout' => -1,
+            'read_write_timeout' => 0,
         ],
 
         'cache' => [
