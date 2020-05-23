4d0b2e549474aca9d3e5ff51056f497b513c0cd5
Nico <nico_orfi@yahoo.com>
2020-05-23 17:34:51 +0200

    UPD: use laravel native cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,8 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ShareUserToView::class,
-            \App\Http\Middleware\Cors::class
+            \App\Http\Middleware\ShareUserToView::class
         ],
         'api' => [
             'throttle:60,1',
--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'ajax/*'],
+    'paths' => ['api/*', 'ajax/*', ''],
 
     'allowed_methods' => ['*'],
 
