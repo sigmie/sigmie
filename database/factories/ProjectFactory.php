f39bc715081b8b8675b48e8af4a0b720cdd2882a
Nico <nico_orfi@yahoo.com>
2020-06-12 09:15:37 +0200

    FIX: Project factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -8,6 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(Project::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'description' => $faker->text(40)
+        'description' => $faker->text(40),
+        'provider' => $faker->randomElement(['google', 'aws', 'digitalocean']),
+        'creds' => encrypt($faker->text(20))
     ];
 });
