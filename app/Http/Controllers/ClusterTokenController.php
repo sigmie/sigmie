2f38d3a2619326d9a12e2c1943955c6e464a34dc
Nico <nico_orfi@yahoo.com>
2020-07-22 13:42:41 +0200

    UPD: test cluster token controller

--- app/Http/Controllers/ClusterTokenController.php
+++ app/Http/Controllers/ClusterTokenController.php
@@ -20,10 +20,9 @@ class ClusterTokenController extends Controller
     public function index(Project $project)
     {
         $cluster = $project->clusters()->first();
-        $tokens = [];
         $plainTextTokens = [self::ADMIN => null, self::SEARCH_ONLY => null];
 
-        if ($cluster->tokens->isEmpty()) {
+        if ($cluster->getAttribute('tokens')->isEmpty()) {
 
             $plainTextTokens[self::ADMIN] = $cluster->createToken(self::ADMIN, ['*'])->plainTextToken;
             $plainTextTokens[self::SEARCH_ONLY] = $cluster->createToken(self::SEARCH_ONLY, ['search'])->plainTextToken;
@@ -31,10 +30,12 @@ class ClusterTokenController extends Controller
             $cluster->refresh();
         }
 
-        foreach ($cluster->tokens as $token) {
+        $clusterId = $cluster->getAttribute('id');
+
+        foreach ($cluster->getAttribute('tokens') as $token) {
             $token = $token->only(['name', 'last_used_at', 'created_at', 'id']);
 
-            $token['cluster_id'] = $cluster->getAttribute('id');
+            $token['cluster_id'] = $clusterId;
 
             if ($token['name'] === self::ADMIN) {
                 $token['active'] = $cluster->getAttribute('admin_token_active');
--- tests/Unit/Controllers/ClusterTokenControllerTest.php
+++ tests/Unit/Controllers/ClusterTokenControllerTest.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 namespace Tests\Unit\Controllers;
 
 use App\Http\Controllers\ClusterTokenController;
+use App\Models\Cluster;
+use App\Models\Project;
+use Faker\Provider\ar_JO\Person;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Inertia\Inertia;
+use Laravel\Sanctum\PersonalAccessToken;
+use Mockery\Mock;
+use PHPUnit\Framework\MockObject\MockObject;
 use Tests\TestCase;
 
 class ClusterTokenControllerTest extends TestCase
@@ -15,9 +23,63 @@ class ClusterTokenControllerTest extends TestCase
      */
     private $controller;
 
+    /**
+     * @var Project|MockObject
+     */
+    private $projectMock;
+
+    /**
+     * @var Collection|MockObject
+     */
+    private $clusterCollectionMock;
+
+    /**
+     * @var Cluster|MockObject
+     */
+    private $clusterMock;
+
+    /**
+     * @var Collection|MockObject
+     */
+    private $tokensCollectionMock;
+
+    /**
+     * @var PersonalAccessToken|MockObject
+     */
+    private $searchTokenMock;
+
+    /**
+     * @var PersonalAccessToken|MockObject
+     */
+    private $adminTokenMock;
+
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->adminTokenMock = $this->createMock(PersonalAccessToken::class);
+        $this->adminTokenMock->method('only')->willReturn([
+            'name' => ClusterTokenController::ADMIN,
+            'last_used_at' => '2000/01/01',
+            'created_at' => '2001/01/01',
+            'id' => 9
+        ]);
+        $this->searchTokenMock = $this->createMock(PersonalAccessToken::class);
+        $this->searchTokenMock->method('only')->willReturn([
+            'name' => ClusterTokenController::SEARCH_ONLY,
+            'last_used_at' => '2020/01/01',
+            'created_at' => '2021/01/01', 'id' => 0
+        ]);
+
+        $this->tokensCollectionMock = $this->createMock(Collection::class);
+
+        $this->clusterMock = $this->createMock(Cluster::class);
+
+        $this->clusterCollectionMock = $this->createMock(Collection::class);
+        $this->clusterCollectionMock->method('first')->willReturn($this->clusterMock);
+
+        $this->projectMock = $this->createMock(Project::class);
+        $this->projectMock->method('clusters')->willReturn($this->clusterCollectionMock);
 
         $this->controller = new ClusterTokenController;
     }
@@ -27,8 +89,35 @@ class ClusterTokenControllerTest extends TestCase
      */
     public function index_renders_inertia_view(): void
     {
-        Inertia::shouldReceive('render')->once()->with('api-token/index');
+        $clusterId = 99;
+        $adminTokenActive = false;
+        $searchTokenActive = true;
+        $this->clusterMock->method('getAttribute')->willReturn($this->tokensCollectionMock, $clusterId, [$this->adminTokenMock, $this->searchTokenMock], $adminTokenActive, $searchTokenActive);
+        $this->tokensCollectionMock->method('isEmpty')->willReturn(false);
 
-        $this->controller->index();
+        $tokens = ['tokens' => [
+            [
+                'name' => ClusterTokenController::ADMIN,
+                'last_used_at' => '2000/01/01',
+                'created_at' => '2001/01/01',
+                'id' => 9,
+                'active' => $adminTokenActive,
+                'cluster_id' => $clusterId,
+                'value' => null
+            ],
+            [
+                'name' => ClusterTokenController::SEARCH_ONLY,
+                'last_used_at' => '2020/01/01',
+                'created_at' => '2021/01/01',
+                'id' => 0,
+                'active' => $searchTokenActive,
+                'cluster_id' => $clusterId,
+                'value' => null
+            ],
+        ]];
+
+        Inertia::shouldReceive('render')->once()->with('token/index', $tokens);
+
+        $this->controller->index($this->projectMock);
     }
 }
