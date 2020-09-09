cdb9e4dee757b0176303d30654086a8dd35c3228
Nico <nico_orfi@yahoo.com>
2020-09-09 09:20:50 +0200

    UPD: log GAE proxy ip, response time, route path

--- app/Http/Middleware/LogRequestInfo.php
+++ app/Http/Middleware/LogRequestInfo.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Middleware;
 
-use Amp\Parallel\Worker\TaskFailureException;
 use Closure;
-use Exception;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 
 class LogRequestInfo
 {
+    private array $ignoredPaths = [
+        'horizon',
+        'nova',
+        'telescope'
+    ];
+
     /**
      * Handle an incoming request.
      *
@@ -21,14 +25,44 @@ class LogRequestInfo
     {
         /** @var  Response */
         $response = $next($request);
+        $ip = $request->server->get('REMOTE_ADDR');
+        $path = $request->getPathInfo();
+        $route = $request->route();
+        $code = $response->getStatusCode();
 
-        Log::info('HTTP Request', [
-            'ip' => $request->getClientIp(),
-            'path' => $request->getPathInfo(),
-            'response_code' => $response->getStatusCode(),
-            'age' => $response->getAge(),
-        ]);
+        if (isset($_SERVER['GAE_SERVICE'])) {
+            $forwardedFor = array_map('trim', explode(',', $request->header('X-Forwarded-For')));
+            $request->server->set('REMOTE_ADDR', $_SERVER['REMOTE_ADDR'] = $forwardedFor[0]);
+        }
+
+        if ($route !== null) {
+            $path = $request->route()->uri();
+        }
+
+        if ($route !== null && $this->isIgnoredPath($path)) {
+            return $response;
+        }
+
+        $responseTime = microtime(true) - LARAVEL_START;
+
+        dispatch(fn () => Log::info('HTTP Request', [
+            'ip' => $ip,
+            'path' => $path,
+            'response_code' => $code,
+            'response_time' => $responseTime
+        ]));
 
         return $response;
+    }
+
+    private function isIgnoredPath($path)
+    {
+        foreach ($this->ignoredPaths as $ignoredPath) {
+            if (preg_match("#^(\/)?{$ignoredPath}#", $path) === 1) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
