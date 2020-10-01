7b6a1618db1651ff56a92729a0017d64e094e3b7
Nico <nico_orfi@yahoo.com>
2020-10-01 11:18:40 +0200

    FIX: disable event autodiscovery and use manual registration

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Events\Cluster\ClusterWasBooted;
+use App\Events\Cluster\ClusterWasCreated;
+use App\Events\Cluster\ClusterWasDestroyed;
+use App\Events\Newsletter\NewsletterSubscriptionWasCreated;
+use App\Listeners\Cluster\PollClusterState;
+use App\Listeners\Notifications\SendClusterDestroyedNotification;
+use App\Listeners\Notifications\SendClusterRunningNotification;
+use App\Listeners\Notifications\SendEmailConfirmationNotification;
+use App\Listeners\Subscription\DispatchUserWasSubscribedEvent;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Laravel\Paddle\Events\WebhookHandled;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -12,17 +22,29 @@ class EventServiceProvider extends ServiceProvider
      * The event listener mappings for the application.
      */
     protected $listen = [
-        // Using auto discovery
+        ClusterWasCreated::class => [
+            PollClusterState::class
+        ],
+        ClusterWasDestroyed::class => [
+            SendClusterDestroyedNotification::class,
+        ],
+        ClusterWasBooted::class => [
+            SendClusterRunningNotification::class
+        ],
+        NewsletterSubscriptionWasCreated::class => [
+            SendEmailConfirmationNotification::class
+        ],
+        WebhookHandled::class => [
+            DispatchUserWasSubscribedEvent::class
+        ]
     ];
 
     /**
      * Determine if events and listeners should be automatically discovered.
-     *
-     * @return bool
      */
     public function shouldDiscoverEvents(): bool
     {
-        return true;
+        return false;
     }
 
     /**
