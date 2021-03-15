1a4284127ed085b0dec4dde66f937cf96c1ea33e
nicoorfi <nico_orfi@yahoo.com>
2021-03-15 15:29:03 +0100

    ADD: debug option to proxy requests

---
+++ app/Http/Middleware/Proxy/ProfileResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware\Proxy;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProfileResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        /** @var Response */
+        $response = $next($request);
+
+        if (
+            app()->bound('debugbar') &&
+            app('debugbar')->isEnabled()
+        ) {
+            $response->setContent(json_encode(app('debugbar')->getData()));
+        }
+
+        return $response;
+    }
+}
