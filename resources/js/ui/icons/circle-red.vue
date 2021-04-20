f6a51eb0b373f18b1f74c8129715a4654264cba8
nicoorfi <nico_orfi@yahoo.com>
2021-04-20 10:36:22 +0200

    ADD: cluster status badges

---
+++ resources/js/ui/icons/circle-red.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    viewBox="0 0 5 5"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  >
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g transform="translate(-27.000000, -6.000000)" fill="#E02020">
+        <circle cx="29.5" cy="8.5" r="2.5"></circle>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- resources/js/views/sidebar/_info.vue
+++ resources/js/views/sidebar/_info.vue
@@ -2,16 +2,37 @@
   <div class="border-gray-600 border-b">
     <div class="px-6 py-2">
       <div class="space-y-1">
-        <div class="text-base leading-5 font-medium text-white" v-if="projectName">{{ projectName }}</div>
-        <div class="text-base leading-5 font-medium text-center text-gray-400" v-else>- no project -</div>
-        <a
-          :href="clusterUrl"
-          v-if="clusterUrl"
-          target="_blank"
-          class="group flex items-center space-x-2.5"
-        >
-          <div class="text-sm leading-5 text-gray-400 font-medium">{{ clusterUrl }}</div>
-        </a>
+        <div class="flex items-center">
+
+          <icon-circle-green class="h-4 w-4 mr-4"></icon-circle-green>
+          <!-- <icon-circle-yellow class="h-4 w-4 mr-4"></icon-circle-yellow> -->
+          <!-- <icon-circle-red class="h-4 w-4 mr-4"></icon-circle-red> -->
+
+          <div>
+            <div
+              class="text-base leading-5 font-medium text-white"
+              v-if="projectName"
+            >
+              {{ projectName }}
+            </div>
+            <div
+              class="text-base leading-5 font-medium text-center text-gray-400"
+              v-else
+            >
+              - no project -
+            </div>
+            <a
+              :href="clusterUrl"
+              v-if="clusterUrl"
+              target="_blank"
+              class="group flex items-center space-x-2.5"
+            >
+              <div class="text-sm leading-5 text-gray-400 font-medium">
+                {{ clusterUrl }}
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
