141d7855487854a97d4cc435320caf7d213c4c03
nicoorfi <nico_orfi@yahoo.com>
2021-04-22 11:35:06 +0200

    WIP: analytics graph

--- resources/js/views/analytics/analytics.vue
+++ resources/js/views/analytics/analytics.vue
@@ -1,8 +1,7 @@
 <template>
   <app>
     <div class="bg-white overflow-hidden shadow rounded-lg">
-      <div class="px-4 py-5 sm:p-6">
-        Analytics
+      <div class="px-4 py-5">
         <canvas id="analytics" width="400" height="400"></canvas>
       </div>
     </div>
@@ -12,6 +11,7 @@
 <script>
 import App from "../layouts/app";
 import { Chart, registerables } from "chart.js";
+import moment from "moment";
 
 Chart.defaults.font.size = 16;
 Chart.register(...registerables);
@@ -24,15 +24,49 @@ export default {
     var ctx = document.getElementById("analytics");
     ctx.height = 100;
 
-    const labels = ["January", "February", "March", "April", "May", "June"];
     const data = {
-      labels: labels,
+      labels: [
+        "Aug 2019",
+        "Sep 2019",
+        "Oct 2019",
+        "Nov 2019",
+        "Dec 2019",
+        "Jan 2020",
+      ],
       datasets: [
         {
           label: "My First dataset",
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
-          data: [0, 10, 5, 2, 20, 30, 45],
+          fill: true,
+          backgroundColor: "rgba(101, 116, 205, 0.1)",
+          borderColor: "rgba(101, 116, 205, 0.8)",
+          borderWidth: 2,
+          data: [
+            0,
+            10,
+            12,
+            35,
+            53,
+            45,
+            45,
+            23,
+            2,
+            34,
+            4,
+            5,
+            7,
+            45,
+            45,
+            23,
+            45,
+            45,
+            23,
+            8,
+            5,
+            2,
+            20,
+            30,
+          ],
+          tension: 0.3,
         },
       ],
     };
@@ -41,15 +75,38 @@ export default {
       type: "line",
       data,
       options: {
+        interaction: {
+          intersect: false,
+          mode: "nearest",
+        },
+        layout: {
+          //   padding: 20,
+          //   padding: {
+          //     left: 50,
+          //   },
+        },
+        elements: {
+          point: {
+            radius: 1,
+          },
+        },
         responsive: true,
         plugins: {
           title: {
-            display: true,
+            display: false,
             text: "Grid Line Settings",
+          },
+          legend: {
+            display: false,
           },
         },
         scales: {
           x: {
+            ticks: {
+              display: true,
+              padding: 15,
+              stepSize: 2,
+            },
             grid: {
               display: true,
               drawBorder: true,
@@ -58,6 +115,11 @@ export default {
             },
           },
           y: {
+            ticks: {
+              display: true, // Hide the left side labels
+              padding: 15,
+              stepSize: 10,
+            },
             grid: {
               display: true,
               drawBorder: true,
