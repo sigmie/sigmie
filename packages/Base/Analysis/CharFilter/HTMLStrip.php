47960078348f342a5ee438191147c9254b4fef95
Nicoorfi <nico@sigmie.com>
2021-09-16 14:09:04 +0200

    UPD: index create validation

--- packages/Base/Analysis/CharFilter/HTMLStrip.php
+++ packages/Base/Analysis/CharFilter/HTMLStrip.php
@@ -8,6 +8,19 @@ use Sigmie\Base\Contracts\CharFilter;
 
 class HTMLStrip implements CharFilter
 {
+    public static function fromRaw(array $raw)
+    {
+        return new static;
+    }
+
+    public function toRaw(): array
+    {
+        return [
+            $this->name => [
+                'type' => 'pattern_replace',
+            ]
+        ];
+    }
     public function name(): string
     {
         return 'html_strip';
