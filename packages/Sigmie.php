78e0488abbd40c3cf66eeb2b6fbcb82feb47a0ea
Nicoorfi <nico@sigmie.com>
2022-11-07 15:20:27 +0200

    fix: sigmie class

--- packages/Sigmie.php
+++ packages/Sigmie.php
@@ -6,22 +6,29 @@ namespace Sigmie;
 
 use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Psr7\Uri;
-use Sigmie\Base\Actions\Index as IndexActions;
+use Sigmie\Index\Actions as IndexActions;
 use Sigmie\Base\APIs\Index;
 use Sigmie\Base\Contracts\ElasticsearchRequest as ElasticsearchRequestInterface;
 use Sigmie\Base\Contracts\ElasticsearchResponse;
-use Sigmie\Base\Contracts\HttpConnection as Connection;
-use Sigmie\Base\Documents\AliveCollection;
-use Sigmie\Base\Http\Connection as HttpConnection;
+use Sigmie\Base\Contracts\ElasticsearchConnection as Connection;
+use Sigmie\Document\AliveCollection;
+use Sigmie\Base\Http\ElasticsearchConnection as HttpConnection;
 use Sigmie\Base\Http\ElasticsearchRequest;
-use Sigmie\Base\Index\AliasedIndex;
-use Sigmie\Base\Index\Builder;
+use Sigmie\Index\AliasedIndex;
+use Sigmie\Index\NewIndex;
 use Sigmie\Base\Search\IndexQueryBuilder;
-use Sigmie\Base\Search\MetricQueryBuilder;
-use Sigmie\Base\Search\SearchBuilder;
+use Sigmie\Base\Analytics\MetricQueryBuilder;
+use Sigmie\Search\SearchBuilder;
 use Sigmie\Http\Contracts\Auth;
 use Sigmie\Http\JSONClient;
+use Sigmie\Parse\FilterParser;
+use Sigmie\Query\Aggs;
+use Sigmie\Query\Contracts\QueryClause;
 use Sigmie\Support\Contracts\Collection;
+use Sigmie\Query\Contracts\Aggs as AggsInterface;
+use Sigmie\Query\Queries\MatchAll;
+use Sigmie\Query\Queries\Query;
+use Sigmie\Search\Search;
 
 class Sigmie
 {
@@ -30,12 +37,12 @@ class Sigmie
 
     public function __construct(Connection $httpConnection)
     {
-        $this->httpConnection = $httpConnection;
+        $this->elasticsearchConnection = $httpConnection;
     }
 
-    public function newIndex(string $name): Builder
+    public function newIndex(string $name): NewIndex
     {
-        $builder = new Builder($this->httpConnection);
+        $builder = new NewIndex($this->elasticsearchConnection);
 
         return $builder->alias($name);
     }
@@ -45,13 +52,15 @@ class Sigmie
         return $this->getIndex($name);
     }
 
-    public function collect(string $name, string $refresh = 'false'): AliveCollection
+    public function collect(string $name, bool $refresh = false): AliveCollection
     {
-        $index = new AliveCollection($name, $refresh);
+        $aliveIndex = new AliveCollection($name, $this->elasticsearchConnection);
 
-        $index->setHttpConnection($this->httpConnection);
+        if ($refresh) {
+            return $aliveIndex->refresh();
+        }
 
-        return $index;
+        return $aliveIndex;
     }
 
     public function refresh(string $name)
@@ -59,24 +68,29 @@ class Sigmie
         return $this->indexAPICall("{$name}/_refresh", 'GET');
     }
 
-    public function query(string $index, string $query): IndexQueryBuilder
-    {
-        $builder = new IndexQueryBuilder($this->search($index));
+    public function search(
+        string $index,
+        Query $query = new MatchAll(),
+        AggsInterface $aggs = new Aggs()
+    ) {
+        $search = new Search($query,$aggs);
 
-        return $builder->query($query);
+        $search->setElasticsearchConnection($this->elasticsearchConnection);
+
+        return $search->index($index);
     }
 
-    public function search(string $index): SearchBuilder
+    public function newSearch(string $index): SearchBuilder
     {
-        return new SearchBuilder($index, $this->httpConnection);
+        return new SearchBuilder($index, $this->elasticsearchConnection);
     }
 
     public function metrics(string $index, string $field)
     {
-        return new MetricQueryBuilder($this->search($index), $field);
+        return new MetricQueryBuilder($this->newSearch($index), $field);
     }
 
-    public function indices(): Collection
+    public function indices(): array
     {
         return $this->listIndices();
     }
@@ -86,7 +100,7 @@ class Sigmie
         try {
             $request = new ElasticsearchRequest('GET', new Uri());
 
-            $res = ($this->httpConnection)($request);
+            $res = ($this->elasticsearchConnection)($request);
 
             return !$res->failed();
         } catch (ConnectException) {
@@ -108,8 +122,8 @@ class Sigmie
         return $this->deleteIndex($index);
     }
 
-    protected function httpCall(ElasticsearchRequestInterface $request): ElasticsearchResponse
+    protected function elasticsearchCall(ElasticsearchRequestInterface $request): ElasticsearchResponse
     {
-        return ($this->httpConnection)($request);
+        return ($this->elasticsearchConnection)($request);
     }
 }
