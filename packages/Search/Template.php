7c58fd8bc755ac7cb87f53f4b9371c4b1818c3d1
Nicoorfi <nico@sigmie.com>
2022-11-07 15:20:54 +0200

    extracts search package

---
+++ packages/Search/Template.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sigmie\Search;
+
+use Sigmie\Base\APIs\SearchTemplate as APIsTemplate;
+use Sigmie\Base\Contracts\DocumentCollection;
+use Sigmie\Base\Contracts\ElasticsearchConnection;
+
+class Template
+{
+    use APIsTemplate;
+
+    public function __construct(protected string $index, protected string $name, ElasticsearchConnection $httpConnection)
+    {
+        $this->setElasticsearchConnection($httpConnection);
+    }
+
+    public function run(array $params): array
+    {
+        return $this->templateAPICall($this->index, $this->name, $params)->json();
+    }
+}
