2b554d67504fbb23b39aef31601c5934f5489505
Nicoorfi <nico@sigmie.com>
2022-11-09 15:44:06 +0200

    add sub repo remotes

--- bin/split.sh
+++ bin/split.sh
@@ -18,23 +18,32 @@ function remote()
 
 git pull origin master
 
-remote filter git@github.com:sigmie/filter.git
-remote cli git@github.com:sigmie/cli.git
-remote http git@github.com:sigmie/http.git
-remote filter git@github.com:sigmie/filter.git
-remote support git@github.com:sigmie/support.git
-remote testing git@github.com:sigmie/testing.git
 remote base git@github.com:sigmie/base.git
+remote document git@github.com:sigmie/document.git
+remote http git@github.com:sigmie/http.git
+remote index git@github.com:sigmie/index.git
+remote mappings git@github.com:sigmie/mappings.git
+remote parse git@github.com:sigmie/parse.git
+remote query git@github.com:sigmie/query.git
+remote search git@github.com:sigmie/search.git
+remote shared git@github.com:sigmie/shared.git
+remote testing git@github.com:sigmie/testing.git
+
 remote english git@github.com:sigmie/english.git
 remote german git@github.com:sigmie/german.git
 remote greek git@github.com:sigmie/greek.git
 
-split 'packages/Filter' filter
+split 'packages/Base' base
+split 'packages/Document' document
+split 'packages/Http' http
+split 'packages/Index' index
+split 'packages/Mappings' mappings
+split 'packages/Parse' parse
+split 'packages/Query' query
+split 'packages/Search' search
+split 'packages/Shared' shared
+split 'packages/Testing' testing
+
 split 'packages/English' english
 split 'packages/German' german
 split 'packages/Greek' greek
-split 'packages/Base' base
-split 'packages/Cli' cli
-split 'packages/Http' http
-split 'packages/Support' support
-split 'packages/Testing' testing
