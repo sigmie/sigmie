d74281e42a755af9ac6b8d0e6785b430669eca78
Nicoorfi <nico@sigmie.com>
2022-12-19 15:42:44 +0200

    optimize tags field

--- packages/Mappings/Types/Tags.php
+++ packages/Mappings/Types/Tags.php
@@ -4,6 +4,27 @@ declare(strict_types=1);
 
 namespace Sigmie\Mappings\Types;
 
-class Tags extends Keyword
+use Sigmie\Index\NewAnalyzer;
+use Sigmie\Mappings\Contracts\Analyze;
+use Sigmie\Query\Queries\Term\Prefix;
+use Sigmie\Query\Queries\Text\Match_;
+
+class Tags extends Text implements Analyze
 {
+    public function analyze(NewAnalyzer $newAnalyzer): void
+    {
+        $newAnalyzer->mapChars(['#' => ''])
+            ->tokenizeOnWordBoundaries()
+            ->lowercase();
+    }
+
+    public function queries(string $queryString): array
+    {
+        $queries = [];
+
+        $queries[] = new Match_($this->name, $queryString);
+        $queries[] = new Prefix($this->name, $queryString);
+
+        return $queries;
+    }
 }
