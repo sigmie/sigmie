1562578d3b6e8ec437f4e85055bd6cd1ae32d30e
Nico Orfanos <nico@sigmie.com>
2023-01-31 09:26:51 +0200

    fix: id field queries

--- packages/Mappings/Types/Id.php
+++ packages/Mappings/Types/Id.php
@@ -4,15 +4,11 @@ declare(strict_types=1);
 
 namespace Sigmie\Mappings\Types;
 
-use Sigmie\Query\Queries\Term\Term;
-
 class Id extends CaseSensitiveKeyword
 {
     public function queries(string $queryString): array
     {
         $queries = [];
-
-        $queries[] = new Term($this->name, $queryString);
 
         return $queries;
     }
