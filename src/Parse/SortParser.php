c5b480ed8537e7ddc2904ce96dd7d97a374135c9
Nico Orfanos <nico@sigmie.com>
2024-07-17 11:19:54 +0300

    feat: geo point sorting

--- src/Parse/SortParser.php
+++ src/Parse/SortParser.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Sigmie\Parse;
 
+use Sigmie\Mappings\Types\GeoPoint;
+use Sigmie\Mappings\Types\Type;
+
 class SortParser extends Parser
 {
     public function parse(string $string): array
@@ -24,9 +27,68 @@ class SortParser extends Parser
                 continue;
             }
 
-            if (str_contains($match, ':')) {
+            if (preg_match(
+                '/(?P<field>\w+)\[(?P<latitude>-?\d+(\.\d+)?),(?P<longitude>-?\d+(\.\d+)?)\]:(?P<unit>\w+):(?P<order>\w+)/',
+                $match,
+                $matches
+            )) {
+
+                $fieldType = $this->properties[$matches['field']];
+
+                if (!$fieldType instanceof GeoPoint) {
+
+                    $this->handleError("Field {$matches['field']} is not a geo point.", [
+                        'field' => $matches['field'],
+                    ]);
+
+                    continue;
+                }
+
+                $field = $matches['field'];
+                $unit = $matches['unit'];
+                $order = $matches['order'];
+                $latitude = $matches['latitude'];
+                $longitude = $matches['longitude'];
+
+                if (!in_array($unit, ['km', 'm', 'cm', 'mm', 'mi', 'yd', 'ft', 'in', 'nmi'])) {
+                    $this->handleError("Invalid unit '{$unit}' for geo distance sort.", [
+                        'unit' => $unit,
+                    ]);
+                    continue;
+                }
+
+                if (!in_array($order, ['asc', 'desc'])) {
+                    $this->handleError("Invalid order '{$order}' for geo distance sort.", [
+                        'order' => $order,
+                    ]);
+                    continue;
+                }
+
+                if (!is_numeric($latitude) || !is_numeric($longitude) || $latitude < -90 || $latitude > 90 || $longitude < -180 || $longitude > 180) {
+                    $this->handleError("Invalid latitude or longitude for geo distance sort.", [
+                        'latitude' => $latitude,
+                        'longitude' => $longitude,
+                    ]);
+                    continue;
+                }
+
+                $sort[] = [
+                    '_geo_distance' => [
+                        $field => [
+                            'lat' => $latitude,
+                            'lon' => $longitude,
+                        ],
+                        "order" => $order,
+                        "unit" => $unit,
+                    ]
+                ];
+
+                continue;
+            } elseif (str_contains($match, ':')) {
+
                 [$field, $direction] = explode(':', $match);
             } else {
+
                 $field = $match;
                 $direction = 'asc';
             }
@@ -38,7 +100,7 @@ class SortParser extends Parser
                 continue;
             }
 
-            $sort[] = [$field => $direction];
+            $sort[] =  [$field => $direction];
         }
 
         return $sort;
--- tests/SortParserTest.php
+++ tests/SortParserTest.php
@@ -8,11 +8,139 @@ use RuntimeException;
 use Sigmie\Document\Document;
 use Sigmie\Mappings\NewProperties;
 use Sigmie\Mappings\Properties;
+use Sigmie\Parse\ParseException;
 use Sigmie\Parse\SortParser;
 use Sigmie\Testing\TestCase;
 
 class SortParserTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function geo_distance_sort_with_valid_unit()
+    {
+        $indexName = uniqid();
+
+        $blueprint = new NewProperties;
+        $blueprint->geoPoint('location');
+
+        $index = $this->sigmie->newIndex($indexName)
+            ->properties($blueprint)
+            ->create();
+
+        $index = $this->sigmie->collect($indexName, true);
+
+        $docs = [
+            new Document([
+                'location' => [
+                    'lat' => 52.49,
+                    'lon' => 13.77,
+                ]
+            ]),
+            new Document([
+                'location' => [
+                    'lat' => 53.49,
+                    'lon' => 13.77,
+                ]
+            ]),
+            new Document([
+                'location' => [
+                    'lat' => 54.49,
+                    'lon' => 13.77,
+                ]
+            ])
+        ];
+
+        $index->merge($docs);
+
+        $props = $blueprint();
+
+        $parser = new SortParser($props);
+
+        $this->expectException(ParseException::class, "Invalid unit 'ieow' for geo distance sort.");
+
+        $query = $parser->parse('location[52.49,13.77]:ieow:asc');
+
+        $this->expectException(ParseException::class, "Invalid order 'foo' for geo distance sort.");
+
+        $query = $parser->parse('location[52.49,13.77]:km:foo');
+
+        $this->expectException(ParseException::class, "Invalid latitude or longitude for geo distance sort.");
+
+        $query = $parser->parse('location[foo,13.77]:km:asc');
+
+        $this->expectException(ParseException::class, "Invalid latitude or longitude for geo distance sort.");
+
+        $query = $parser->parse('location[91,13.77]:km:asc');
+    }
+
+    /**
+     * @test
+     */
+    public function geo_distance_sort()
+    {
+        $indexName = uniqid();
+
+        $blueprint = new NewProperties;
+        $blueprint->geoPoint('location');
+
+        $index = $this->sigmie->newIndex($indexName)
+            ->properties($blueprint)
+            ->create();
+
+        $index = $this->sigmie->collect($indexName, true);
+
+        $docs = [
+            new Document([
+                'location' => [
+                    'lat' => 52.49,
+                    'lon' => 13.77,
+                ]
+            ]),
+            new Document([
+                'location' => [
+                    'lat' => 53.49,
+                    'lon' => 13.77,
+                ]
+            ]),
+            new Document([
+                'location' => [
+                    'lat' => 54.49,
+                    'lon' => 13.77,
+                ]
+            ])
+        ];
+
+        $index->merge($docs);
+
+        $props = $blueprint();
+
+        $parser = new SortParser($props);
+
+        $query = $parser->parse('location[52.49,13.77]:km:asc');
+
+        $res = $this->sigmie->query($indexName)
+            ->addRaw('sort', $query)
+            ->get();
+
+        $this->assertTrue($res->json('hits.hits')[0]['_source']['location']['lat'] === 52.49);
+        $this->assertTrue($res->json('hits.hits')[1]['_source']['location']['lat'] === 53.49);
+        $this->assertTrue($res->json('hits.hits')[2]['_source']['location']['lat'] === 54.49);
+
+        $parser = new SortParser($props);
+
+        $query = $parser->parse('location[52.49,13.77]:km:desc');
+
+        $res = $this->sigmie->query($indexName)
+            ->addRaw('sort', $query)
+            ->get();
+
+        $this->assertTrue($res->json('hits.hits')[0]['_source']['location']['lat'] === 54.49);
+        $this->assertTrue($res->json('hits.hits')[1]['_source']['location']['lat'] === 53.49);
+        $this->assertTrue($res->json('hits.hits')[2]['_source']['location']['lat'] === 52.49);
+    }
+
+
     /**
      * @test
      */
